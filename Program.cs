8afdc99eefa5c511f89f532cea8f3e1e58cd667d
randomchannel465-oss <randomchannel465@gmail.com>
2026-05-10 00:36:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,15 +3,12 @@ using Microsoft.Extensions.FileProviders;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseSwagger();
-
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
@@ -19,10 +16,16 @@ app.UseHttpsRedirection();
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
-        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+        Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "wwwroot")),
     RequestPath = ""
 });
 
 app.MapControllers();
 
-app.Run();
+var port =
+    Environment.GetEnvironmentVariable("PORT")
+    ?? "8080";
+
+app.Run($"http://0.0.0.0:{port}");
